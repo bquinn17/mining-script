5624db5e0353b6867b91cef3c8ccf69a319811cb
Bryan Quinn <blq9064@rit.edu>
2018-09-10 14:59:33 -0400

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-cd /bin && wget -O miner.deb https://download.minergate.com/deb-cli
-sudo apt install ./miner.deb
+cd /bin
+wget -O minergate-cli-release.deb https://download.minergate.com/deb-cli
+sudo apt install ./minergate-cli-release.deb
 minergate-cli -u quinnkid12321@gmail.com --xmr 6 &
